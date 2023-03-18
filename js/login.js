8246c1f0ecb8e46564870256102971f249791d2e
Wajeeh Azeem <108671952+AzeemX-dev@users.noreply.github.com>
2023-03-18 18:17:38 +0500

    Keep track of username after login

--- js/login.js
+++ js/login.js
@@ -5,9 +5,12 @@ const User = require("./user");
 
 router.post("/login.html", async (req, res) => {
   const user = await User.findOne({ name: req.body.name });
+  
+
   if (user) {
     const isPasswordValid = await bcrypt.compare(req.body.password, user.password);
     if (isPasswordValid) {
+      req.session.username = user.name;
       res.redirect(`/welcomeUser.html?username=${user.name}`);
     } else {
       res.send('Invalid username or password');
