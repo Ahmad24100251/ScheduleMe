17d2699ae945fffbb394d522d0cf0bbc869eda46
Wajeeh Azeem <108671952+AzeemX-dev@users.noreply.github.com>
2023-04-05 16:01:12 +0500

    Update addTasks.js

--- js/addTasks.js
+++ js/addTasks.js
@@ -8,21 +8,16 @@ router.post("/addTasks.html", async (req, res) => {
     const taskName = req.body.task;
     const priority = req.body.priority;
     const hours = req.body.hours;
-
     // get the current task count for the user
     const taskCount = await Task.countDocuments({ username: username });
-
     // check if the user has reached the task limit
     if (taskCount >= 17) {
       return res.status(400).send("Task limit reached");
     }
-
     // calculate the start and end times for the new task
     const startAt = taskCount === 0 ? new Date('2023-04-01T06:00:00.000Z') : (await Task.findOne({ username: username, taskNumber: taskCount })).endAt;
     const endAt = new Date(startAt.getTime() + hours * 60 * 60 * 1000);
-
     const taskNumber = taskCount + 1; // increment the task count to get the new task number
-
     const task = new Task({
       username,
       taskNumber,
