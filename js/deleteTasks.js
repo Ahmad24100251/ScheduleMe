feecf449f990fe4befcb2da8184d4d6e57afc88a
Wajeeh Azeem <108671952+AzeemX-dev@users.noreply.github.com>
2023-04-05 16:01:35 +0500

    Update deleteTasks.js

--- js/deleteTasks.js
+++ js/deleteTasks.js
@@ -7,12 +7,12 @@ const router = express.Router();
 // Delete all tasks at 3:49 PM every day
 //AT 10:00 PM -> 0 22
 //AT 12:00 -> 0 0
-cron.schedule('00 22 * * *', async () => { // 3:49PM is represented as 15:49 in 24-hour clock notation
+cron.schedule('09 2 * * *', async () => { // 3:49PM is represented as 15:49 in 24-hour clock notation
   try {
     const cutoffDate = new Date(Date.now() - (24 * 60 * 60 * 1000)); // Delete tasks older than 24 hours
     await Task.deleteMany({ startAt: { $lt: cutoffDate } });
 
-    console.log('All tasks deleted at 10:00 PM');
+    console.log('All tasks deleted');
   } catch (error) {
     console.error(error);
   }
