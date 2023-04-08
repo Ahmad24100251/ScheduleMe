91ab3e51dee9845fade21ca7343edf8d387b5999
Wajeeh Azeem <108671952+AzeemX-dev@users.noreply.github.com>
2023-04-08 07:24:28 +0500

    Update addRecord.js

--- js/addRecord.js
+++ js/addRecord.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const Record = require("./records");
+const Account = require("./account");
 
 router.post("/addRecord.html", async (req, res) => {
   try {
     const username = req.session.username;
     const spending = parseInt(req.body.spending);
+    const accountName = req.body.account;
+
+    // Find the account to deduct from
+    const accountToDeductFrom = await Account.findOne({ username, accountName });
+    if (!accountToDeductFrom) {
+      throw new Error("Account not found");
+    }
+
+    // Deduct the spending from the account's balance
+    accountToDeductFrom.balance -= spending;
+    await accountToDeductFrom.save();
 
     // Check if the user already has a spending record
     let existingRecord = await Record.findOne({ username });
